b73d0225ffcbc3f0e311ae6f909f5fee03af2fe5
maciejspychala <mac.spychala@gmail.com>
2018-12-31 12:00:34 +0000

    simple todo

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hello'
+        title: 'todo list',
+        todos: [
+            { text: 'git init' },
+            { text: 'git add -A' },
+            { text: 'git commit' }
+        ],
+        newTodo: ''
+    },
+    methods: {
+        addTodo: function() {
+            this.todos.push({ text: this.newTodo });
+            this.newTodo = '';
+        }
     }
 });
